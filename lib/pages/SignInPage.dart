970158725b898f9cbe76ae22956482b9a4dddcdf
Kirthik24 <kirthikcode@gmail.com>
2022-09-08 13:16:18 +0530

    google sign in info got

--- lib/pages/SignInPage.dart
+++ lib/pages/SignInPage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:skct_chats/main.dart';
+import 'package:skct_chats/pages/GoogleInfo.dart';
 
 
 
@@ -44,7 +45,7 @@ Widget build(BuildContext context) {
          Future<UserCredential> user = signInWithGoogle();
          Navigator.push(
            context,
-           MaterialPageRoute(builder: (context) => UserInformation()),
+           MaterialPageRoute(builder: (context) => GoogleInfo()),
          );
         },
       ),
